7291d554808b69ea6bbe72d7bc5ddd1a7d99df43
Kesandu123 <ukpai.kesandu@gmail.com>
2025-10-15 20:49:55 +0100

    changed db

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 export const connectDB = async():Promise<void> => {
     try{
-        console.log("MONGO_URI:", process.env.MONGODB);
+        console.log("MONGO:", process.env.MONGODB);
         await mongoose.connect(process.env.MONGODB);
         console.log("connection successful")
     }
